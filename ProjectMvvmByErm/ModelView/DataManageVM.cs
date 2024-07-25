5bf0ddd59c76763d5b132d5d540a95bb9391affb
Владислав Сарматов <iosglad@yandex.ru>
2024-07-25 17:44:24 +0400

    Комманда создания отдела

--- ProjectMvvmByErm/ModelView/DataManageVM.cs
+++ ProjectMvvmByErm/ModelView/DataManageVM.cs
@@ -8,6 +8,8 @@ using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ProjectMvvmByErm.ModelView
 {
@@ -36,6 +38,36 @@ namespace ProjectMvvmByErm.ModelView
             get { return allPositions; }
             set { allPositions = value; NotifyPropertyChanged("AllPositions"); }
         }
+
+        public string DepartmentName { get; set; }
+
+        #region КОММАНДЫ СОЗДАНИЯ, УДАЛЕНИЕ И ИЗМЕНЕНИЯ ПОЗИЦИЙ
+
+        private RelayCommand addNewDepartment;
+        public RelayCommand AddNewDepartment
+        {
+            get
+            {
+                return addNewDepartment ?? new RelayCommand(obj =>
+                {
+                    Window win = obj as Window;
+                    string resultStr = "";
+                    if (DepartmentName == null || DepartmentName.Replace(" ", "").Length == 0)
+                    {
+                        SetRedBlockControll(win, "NameTextBlock");
+                    }
+                    else
+                    {
+                        resultStr = DataWorker.CreateDepartment(DepartmentName);
+                        win.Close();
+                    }
+                }
+                );
+            }
+        }    
+
+
+        #endregion
 
         #region КОММАНДЫ ОТКРЫТИЯ ОКОН
         private RelayCommand openAddNewUserWin;
@@ -105,6 +137,12 @@ namespace ProjectMvvmByErm.ModelView
             window.ShowDialog();
         }
         #endregion
+
+        private void SetRedBlockControll(Window win, string blockName)
+        {
+            Control block = win.FindName(blockName) as Control;
+            block.BorderBrush = Brushes.Red;
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged(string propertyName)
